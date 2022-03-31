dc9b26949d358ebd71b68d29994f8b523141d46f
dejavu518 <cf_1118sab@163.com>
2022-03-31 17:17:38 +0800

    refactor:重写App和main.js

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
+  <!-- 最大的App组件 -->
   <div id="app" class="wrapper">
     <keep-alive exclude="Detail">
-      <router-view/>
+      <router-view />
     </keep-alive>
-    <main-tab-bar/>
+    <main-tab-bar />
   </div>
 </template>
 
 <script>
-  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
-  export default {
-    name: 'app',
-    components: {
-      MainTabBar
-    }
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
   }
+}
 </script>
 
 <style>
-  @import "assets/css/base.css";
+@import "assets/css/base.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
+// 入口文件
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false //阻止启动生产模式
 Vue.prototype.$bus = new Vue()
-
 new Vue({
   render: h => h(App),
   router
 }).$mount('#app')
-
-
