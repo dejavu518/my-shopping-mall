818b247c89dd2b22851f74a02ee93d414eeeecab
dejavu518 <cf_1118sab@163.com>
2022-04-01 11:14:34 +0800

    feat:tab-control

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- ********滚动内容区域******** -->
   <div class="wrapper" ref="wrapper">
     <div class="content">
       <slot></slot>
@@ -7,67 +8,35 @@
 </template>
 
 <script>
-  import BScroll from 'better-scroll'
-
-  export default {
-    name: "Scroll",
-    props: {
-      probeType: {
-        type: Number,
-        default: 0
-      },
-      pullUpLoad: {
-        type: Boolean,
-        default: false
-      }
+import BScroll from 'better-scroll'
+export default {
+  name: 'Scroll',
+  props: {
+    probeType: {
+      type: Number,
+      dafault: 0
     },
-    data() {
-      return {
-        scroll: null,
-        message: '哈哈哈'
-      }
-    },
-
-    mounted() {
-      // 1.创建BScroll对象
-      this.scroll = new BScroll(this.$refs.wrapper, {
-        click: true,
-        probeType: this.probeType,
-        pullUpLoad: this.pullUpLoad
-      })
-
-      // 2.监听滚动的位置
-      if (this.probeType === 2 || this.probeType === 3) {
-        this.scroll.on('scroll', (position) => {
-          // console.log(position);
-          this.$emit('scroll', position)
-        })
-      }
-
-      // 3.监听scroll滚动到底部
-      if (this.pullUpLoad) {
-        this.scroll.on('pullingUp', () => {
-          this.$emit('pullingUp')
-        })
-      }
-    },
-    methods: {
-      scrollTo(x, y, time=300) {
-        this.scroll && this.scroll.scrollTo(x, y, time)
-      },
-      refresh() {
-        this.scroll && this.scroll.refresh()
-      },
-      finishPullUp() {
-        this.scroll && this.scroll.finishPullUp()
-      },
-      getScrollY() {
-        return this.scroll ? this.scroll.y : 0
-      }
+    pullUpload: {
+      type: Boolean,
+      default: false
     }
+  },
+  data() {
+    return {
+      scroll: null,
+      message: '哈哈哈'
+    }
+  },
+  mounted() {
+    // 1.创建BScroll对象
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType,
+      pullUpload: this.pullUpload
+    })
   }
+}
 </script>
 
 <style scoped>
-
 </style>
